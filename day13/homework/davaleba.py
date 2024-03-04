d76a5c30853727dad2c5bad383762d2ffee5d21a
SabaTetemadze <tetemadzesaba228@gmail.com>
2024-03-04 23:06:04 +0400

    commit

---
+++ day13/homework/davaleba.py
@@ -0,0 +1,36 @@
+# 1)Write a program that prints numbers from 1 to 10 using a while loop
+i = 0
+while i <= 19:
+    i = i + 1
+    if True:
+        print(i)
+
+
+# 2)Write a program that prints the even numbers from 1 to 10 using a for loop
+
+for i in range(0,10):
+    i = i + 1
+    print(i)
+
+
+# 3)Write a program that asks the user to enter a number and then prints whether the number is positive, negative, or zero using an if-else statement
+
+i = 0  #i - მომხმარებლის არჩეული რიცხვი
+if i <= 0:
+    print("Negative")
+if i == 0:
+    print("Zero")
+if i > 0:
+    print("Positive")
+
+
+# 4)Write a program that asks the user to enter a password. If the password is "abc123", print "Access granted"; otherwise, print "Access denied"
+
+i = "ac123"
+if i == "abc123":
+    print("Access granted")
+if i != "abc123":
+    print("Access denied")
+
+
+# 5)Write a program that prints numbers from 1 to 10, but stops if the number is 5 using a while loop and the break statement. break
