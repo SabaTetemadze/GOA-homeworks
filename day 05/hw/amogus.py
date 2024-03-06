7ce4c0a9b2bf61cfd8fdafee4a2dcf0ed6f944c2
SabaTetemadze <tetemadzesaba228@gmail.com>
2024-03-06 23:07:16 +0400

    commit

---
+++ day 05/hw/amogus.py
@@ -0,0 +1,63 @@
+message = "debugging"
+print(message)
+
+
+
+print(5+3)
+
+
+
+salary = 5000
+role ="Analist"
+Age = 29
+print(salary)
+
+
+
+brand = "Audi"
+model = "Q5"
+print(model)
+
+
+
+name = "Anna"
+score = 96
+print(name)
+print(score)
+
+
+
+#Storing the flight number
+
+flight_n = "BA0117"
+
+#Storing the flight information
+
+destination = "New York"
+distance = 1580
+print(flight_n)
+print(destination)
+print(distance)
+
+
+
+p1 = "  *  "
+p2 = " *** "
+p3 = "*****"
+
+# modify the code
+
+print(p1)
+print(p2)
+print(p3)
+print(p2)
+print(p1)
+
+
+
+salary = 150000
+pay_raise = 10000
+new_salary = salary + pay_raise
+# Create a variable named new_salary 
+
+print(new_salary)
