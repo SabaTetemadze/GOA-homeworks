e57062884b99f3f9c734ab203b9646f598668772
SabaTetemadze <tetemadzesaba228@gmail.com>
2024-03-07 18:09:35 +0400

    commit

---
+++ day 06/hw 06/შრეკი.py
@@ -0,0 +1,11 @@
+#name = input("what is your name?: ")
+
+#print(name)
+"""პირველ ხაზზე დავადეკლარირეთ string ტიპის ცვლადი სახელად name რომლის მნიშვნელობა
+უნდა მიანიჭოს მომხმარებელმა დამეორე ხაზზე გამოვიძახეთ ეს ცვლადი და გამოვიტანეთ
+მისი მნიშვნელობა print ფუნქციის საშუალებით"""
+
+
+Name = input("enter your name ")
+
+print("user enterede value, " + Name)
