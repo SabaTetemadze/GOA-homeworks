2b7b311de1b64d4984f5347fd12330f8c1c00088
SabaTetemadze <tetemadzesaba228@gmail.com>
2024-03-08 17:20:08 +0400

    commit

---
+++ day 04/hw 04/sus.py
@@ -0,0 +1,13 @@
+mother= 37
+father= 40
+sister= 16
+
+#gavida 2 weli
+
+mother= 37+2
+father= 40+2
+sister= 16+2
+
+print(mother)
+print(father)
+print(sister)
---
+++ day 07/hw 07/hw 07.py
@@ -0,0 +1,31 @@
+mom_age = 37
+dad_age = 40
+sister_age = 16
+my_age = 14
+
+mom_work = "მუშაობს ბუჟალტრად"
+dad_work = "მუშაობს ტაქსისტად"
+sister_study = "მე-11 კალსში"
+me_study = "მე-9 კლასში"
+
+print(type(mom_age))
+print(type(dad_age))
+print(type(sister_age))
+print(type(my_age))
+print(" ")
+print(type(mom_work))
+print(type(dad_work))
+print(type(sister_study))
+print(type(me_study))
+print(" ")
+
+print("ჩემი დედა არის " + str(mom_age) + " წლის")
+print("ჩემი მამა არის " + str(dad_age) + " წლის")
+print("ჩემი და არის " + str(sister_age) + " წლის")
+print("მე ვარ " + str(my_age) + " წლის")
+print(" ")
+
+print("ჩემი დედეა " + mom_work)
+print("ჩემი მამა " + dad_work)
+print("ჩემი და არის " + sister_study)
+print("მე ვარ " + me_study)
